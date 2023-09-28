206cbf47124c2078e3867591fda112e1eb948522
Linf0rd <linfordsnagger@gmx.com>
2023-09-28 15:44:44 +0200

    Add bubble_sort function to sort.h
    Adding the declaration of the 'bubble_sort' function to sort.h. This function is used for sorting an array of integers in ascending order using the Bubble sort algorithm.

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 /**
- * struct listint_s Doubly linked list node.
+ * struct listint_s - Doubly linked list node.
  *
  * @n: Integer stored in the node.
  * @prev: Pointer to the previous element of the list.
